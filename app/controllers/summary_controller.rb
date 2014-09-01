f7e2d106a1843e3caafdb0d7677a80cb9f8b7648
Steven Orvis <steven@trappingpa.com>
2014-09-01 15:40:42 -0400

    Added tests to summary_controller to check for latest temperature reading

--- app/controllers/summary_controller.rb
+++ app/controllers/summary_controller.rb
@@ -1,5 +1,5 @@
 class SummaryController < ApplicationController
   def index
-    @latest = TemperatureReading.new :CelciusReading =>  9.9
+    @latest = TemperatureReading.latest
   end
 end
--- spec/controllers/summary_controller_spec.rb
+++ spec/controllers/summary_controller_spec.rb
@@ -3,10 +3,22 @@ require 'rails_helper'
 RSpec.describe SummaryController, :type => :controller do
 
   describe "GET index" do
+    before(:each) do
+      TemperatureReading.create! :CelciusReading => 4
+    end
     it "returns http success" do
       get :index
       expect(response).to be_success
     end
-  end
 
+    it 'should set @latest' do
+      get :index
+      assigns(:latest).should_not be_nil
+    end
+
+    it 'should call TemperatureReading.latest' do
+      TemperatureReading.should_receive(:latest)  
+      get :index
+    end
+  end
 end
